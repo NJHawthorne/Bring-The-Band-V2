5eef3d90179ccc16081de118a7fcddd195a850cc
Nate Hawthorne <natejhawthorne@gmail.com>
2016-06-16 23:24:39 -0500

    Added search function to search page using Spotify api

--- app/scripts/Pages/SearchPage.js
+++ app/scripts/Pages/SearchPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import $ from 'jquery';
 import Navigation from './../Subcomponents/Navigation.js';
 import votedBands from './../Collections/VotedBandsCollection.js';
 
@@ -6,27 +7,48 @@ export default React.createClass({
 	getInitialState: function() {
 		console.log('getInitialState');
 		return {
-			votedBands: votedBands
+			votedBands: votedBands,
+			searchResults: []
 		}
 	},
 	componentDidMount: function() {
-		console.log('componentWillMount');
+		console.log('componentDidMount');
 		this.state.votedBands.on('update change', () => {
 			this.setState({votedBands: this.state.votedBands})
 		});
 		votedBands.fetch();
-
 	},
 	componentWillUnmount: function() {
+		console.log('componentWillUnmount');
 		this.state.votedBands.off('update change');
 	},
 	render: function() {
 		console.log('render');
+		console.log(this.state.searchResults);
 		return (
 			<section>
 				<Navigation />
 				<div>Search for things!</div>
+				<form
+					onSubmit={this.handleSearch}>
+					<input
+						type='text'
+						placeholder='Band name...'
+						ref='searchedBand' />
+					<input
+						type='submit' />
+				</form>
 			</section>
 		);
+	},
+	handleSearch: function(e) {
+		e.preventDefault();
+		console.log(this.refs.searchedBand.value);
+		let searchedBand = this.refs.searchedBand.value;
+		this.searchRequest = $.get("https://api.spotify.com/v1/search?q="+searchedBand+"*&type=artist", function(bandData) {
+			this.setState({
+				searchResults: bandData.artists.items
+			});
+		}.bind(this));
 	}
 });
